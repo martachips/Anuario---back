0f1d9adcbd09fdd780baa4f07183c21a813665a1
Viru <viraschia@gmail.com>
2024-06-15 14:22:01 +0200

    arquitectura basica y conexion con mongo db

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const { connectDB } = require('./src/config/db');
 
 const app = express();
+connectDB();
+
+app.use(express.json());
 
 app.listen(3000, (req, res, next) => {
   console.log('Escuchando en el puerto http://localhost:3000');
